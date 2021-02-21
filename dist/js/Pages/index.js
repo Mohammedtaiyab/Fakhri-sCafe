cc2d99448dba351013d1355c19c60b14d08e67e9
Mohammedtaiyab <55629150+Mohammedtaiyab@users.noreply.github.com>
2021-02-22 00:09:17 +0530

    new

---
+++ dist/js/Pages/index.js
@@ -0,0 +1,15 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAa5UX0Hyg8MEOgkDttlsXsWZZcgWvM2AM",
+    authDomain: "fakhri-s-cafe.firebaseapp.com",
+    databaseURL: "https://fakhri-s-cafe-default-rtdb.firebaseio.com",
+    projectId: "fakhri-s-cafe",
+    storageBucket: "fakhri-s-cafe.appspot.com",
+    messagingSenderId: "737693118780",
+    appId: "1:737693118780:web:47c4da88e97c31e3992e39",
+    measurementId: "G-2F00Z2NBC0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
